a1e79a61d469768e57a2c6a0e4fa5ef65ea882d5
eduardo <eduardo@eduardo-K53SD.(none)>
2013-10-17 19:03:14 +0200

    comenzando la clase 1.0

--- ceca.php
+++ ceca.php
@@ -7,9 +7,29 @@ namespace Ubublog\Ceca;
 * @since 1.0.0
 */
 class Ceca{
+	/**	 
+	 * @var integer $_merchantID Requerido 9 Identifica al comercio. Facilitado por la caja en el proceso de alta
+	 */
+	protected $_merchantID;
+	
+	/**
+	 * @var integer $_acquirerBIN Requerido 10 Identifica la caja. Facilitado por la caja en el proceso de alta.
+	 */
+	protected $_acquirerBIN;
+
+	/**
+	 * @var integer $_terminalID Requerido 8 Identifica al terminal. Facilitado por la caja en el proceso de alta.
+	 */
+	protected $_terminalID;
+
+	/**
+	 * 
+	 * @var string Num_operacion Requerido 50 Identifica para el comercio la operación, nº de pedido, factura,  albaran, etc.… Puede ser alfanumérico pero están prohibidos los caracteres extraños típicos como ¿,?,%,&,*,etc.
+	 */
+	protected $_num_operacion;
+
 	protected $_urlok;
 	protected $_urlnok;
-
 	public function __construct()
 	{
 
