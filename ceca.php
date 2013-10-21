d0ddf8268c465c2b68bb90e8a5bdddafcd0d6682
eduardo <eduardo@eduardo-K53SD.(none)>
2013-10-21 09:55:34 +0200

    seguimos avanzando

--- ceca.php
+++ ceca.php
@@ -28,11 +28,37 @@ class Ceca{
 	 */
 	protected $_num_operacion;
 
-	protected $_urlok;
-	protected $_urlnok;
+	/**	 
+	 * @var float Importe Requerido 12 Importe de la operación sin formatear. Siempre será un número entero donde los dos últimos dígitos serán los céntimos de Euro.
+	 */
+	protected $_importe;
+
+	/**
+	 * @var string TipoMoneda Requerido 3 Es el código ISO-4217 correspondiente a la moneda en la que se  efectúa el pago. Contendrá el valor 978 para Euros.
+	 */
+	protected $_tipoMoneda;
+
+	/**
+	 * @var integer Exponente Requerido 1 Actualmente siempre será 2
+	 */
+	protected $_exponente;
+
+	/**
+	 * Esta URL no deberá utilizarse  para actualizar la operación como pagada en el servidor del  comercio. Ver más información al final de la tabla.
+	 * @var string URL_OK Requerido 500 URL completa. Es la URL determinada por el comercio a  la que Cecabank devolverá el control en el caso de que la  operación finalice correctamente.
+	 */
+	protected $_url_ok;
+
+	/**
+	 * 
+	 * @var string URL_NOK Requerido 500 URL completa. Es la URL determinada por el comercio a  la que Cecabank devolverá el control en el caso de que la  operación no pueda realizarse por algún motivo.
+	 */
+	protected $_url_nok;
+
+
 	public function __construct()
 	{
-
+		$this->_exponente = 2;
 	}
 
 }
